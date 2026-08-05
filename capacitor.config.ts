305a246c272d41b08c1e5353cc7cb5e30a7e42fd
jasn4558-crypto <jasn4558crypto@gmail.com>
2026-08-05 00:17:33 -0600

    Fase 9: Integración de Capacitor y generación de proyecto nativo para Android

---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tiendaveronica.app',
+  appName: 'Tienda Verónica',
+  webDir: 'public',
+  server: {
+    url: 'https://pagina-web-vero.vercel.app/',
+    cleartext: true
+  }
+};
+
+export default config;
